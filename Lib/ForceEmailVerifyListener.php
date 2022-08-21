e0eadcddc34c8b7a1c971669425014a4ad5c7634
Scott Koranda <skoranda@gmail.com>
2022-08-21 11:55:59 -0500

    Better event listener strategy
    Change the event listener to fire for controller beforeRedirect
    events and then redirect to the CoInvites controller with the
    verifyEmailAddress action and pass the ID of the email just
    edited. That will display the usual flash for the user
    and kick off the email verification flow. The final redirect
    is back to the CoPerson canvas.

--- Lib/ForceEmailVerifyListener.php
+++ Lib/ForceEmailVerifyListener.php
@@ -7,20 +7,37 @@ class ForceEmailVerifyListener implements CakeEventListener {
 
  public function implementedEvents() {
    return array(
-     'Model.afterSave' => 'redirectVerifyEmailAddress'
+     'Controller.beforeRedirect' => 'redirectVerifyEmailAddress'
    );
  }
 
  public function redirectVerifyEmailAddress(CakeEvent $event) {
-   // The subject of the event is a Model object.
-   $model = $event->subject();
+   // The subject of the event is a Controller object.
+   $controller = $event->subject();
 
-   // We only intercept the EmailAddress model.
-   if(!($model->name === 'EmailAddress')) {
+   // We only intend to intercept the EmailAddressesController.
+   if(!($controller->name === 'EmailAddresses')) {
      return true;
    }
 
-   $emailAddress = $model->data;
+   // We only intend to intercept the edit action.
+   if(!($controller->action === "edit")) {
+     return true;
+   }
+
+   // We only intend to intercept the PUT method.
+   $method = $controller->request->method();
+   if(!($method === 'PUT')) {
+     return true;
+   }
+
+   // We do not intercept any REST calls.
+   $restful = $controller->request->is('restful');
+   if($restful) {
+     return true;
+   }
+
+   $emailAddress = $controller->data;
 
    // We only intercept EmailAddresses attached to CoPerson records.
    if(empty($emailAddress['EmailAddress']['co_person_id'])) {
@@ -29,29 +46,16 @@ class ForceEmailVerifyListener implements CakeEventListener {
 
    $coPersonId = $emailAddress['EmailAddress']['co_person_id'];
 
-   // We only intercept edits by checking revision and deleted fields.
-   if(!empty($emailAddress['EmailAddress']['deleted'])) {
-     return true;
-   }
-
-   if($emailAddress['EmailAddress']['revision'] == 0) {
-     return true;
-   }
-
-   // Bail if there is no actor_identifier field.
-   if(empty($emailAddress['EmailAddress']['actor_identifier'])) {
-     return true;
-   }
-
-   $actorIdentifier = $emailAddress['EmailAddress']['actor_identifier'];
-
    // Pull the full CoPerson record and associated OrgIdentities and their
+   // Identifiers.
    $args = array();
    $args['conditions']['CoPerson.id'] = $coPersonId;
    $args['contain'] = array();
    $args['contain']['CoOrgIdentityLink']['OrgIdentity'] = 'Identifier';
 
-   $coPerson = $model->CoPerson->find('first', $args);
+   $coPerson = $controller->EmailAddress->CoPerson->find('first', $args);
+
+   $actorIdentifier = $controller->Session->read('Auth.User.username');
 
    // We should only fire if the actor is the CoPerson.
    $verify = false;
@@ -66,11 +70,27 @@ class ForceEmailVerifyListener implements CakeEventListener {
    }
 
    if($verify) {
-     $email = $emailAddress['EmailAddress']['mail'];
-     $model->CoInvite->send($coPersonId, $orgIdentityId, $coPersonId, $email, null, 'Users',   _txt('em.invite.subject.ver'), _txt('em.invite.body.ver'), $emailAddress['EmailAddress']['id']);
+     // We redirect to the CoInvites controller with the verifyEmailAddress
+     // action and pass the EmailAddress ID to verify.
+     $url = array();
+     $url['plugin'] = null;
+     $url['controller'] = 'co_invites';
+     $url['action'] = 'verifyEmailAddress';
+     $url['email_address_id'] = $emailAddress['EmailAddress']['id'];
+
+     // We need to set the response details here because we are going
+     // to stop propagation of the CakeEvent and therefore cause the
+     // redirect() method of the controller that was in process to
+     // short circuit before setting the header and status.
+     $controller->response->header('Location', Router::url($url, true));
+     $controller->response->statusCode(302);
+     $controller->response->send();
+
+     // Stop the event propagation.
+     $event->stopPropagation();
    }
 
-   // Always return true so as not to interfere with the save action.
+   // Always return true so as not to interfere with call stack.
    return true;
  }
 }
